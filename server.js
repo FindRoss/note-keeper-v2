83abaf87b74334107bc09d7fba2efc9703a030b2
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2023-01-21 10:48:45 +0100

    remove gitignore files

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/note"));
+
 // get driver connection
 const dbo = require("./config/db");
+
+//* Serve static assets in production, must be at this location of this file
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(port, () => {
   // perform a database connection when server starts
